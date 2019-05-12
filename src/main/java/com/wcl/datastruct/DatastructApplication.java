cddceafb2613a2da051957687ae566b944cfadcc
wangchanglei <459342533@qq.com>
2019-05-12 22:07:37 +0800

    first commit

---
+++ src/main/java/com/wcl/datastruct/DatastructApplication.java
@@ -0,0 +1,13 @@
+package com.wcl.datastruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wcl/datastruct/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wcl.datastruct;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DatastructApplication.class);
+	}
+
+}
